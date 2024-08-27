7ff418fddcfc7a55dd23d70e09260d850b2260ee
QW <loh.qwen@gmail.com>
2024-08-27 18:07:31 +0800

    2024 Update: Removing About Section. [Note: package-lock.json overwritten]

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
       <div id="more-info-wrapper">
         <Skills />
         <Portfolio popUp={pop} />
-        <About />
+        {/* <About /> */}
       </div>
     </div>);
 
